6f6329da3d1c9d5190ff1a8c6adb7ec5f7548411
Samuel Čavoj <samuel@cavoj.net>
2020-07-06 02:03:22 +0200

    Check python version on startup

--- picomc/__init__.py
+++ picomc/__init__.py
@@ -1,5 +1,17 @@
-# This is referenced in setup.py
-from picomc.main import main
-
 from ._version import __version__
 del _version
+
+MINPYVERSION = (3, 7, 0)
+
+import sys
+
+if sys.version_info < MINPYVERSION:
+    print("picomc, version {}".format(__version__))
+    print(
+        "picomc requires at least Python version "
+        "{}.{}.{}. You are using {}.{}.{}.".format(*MINPYVERSION, *sys.version_info)
+    )
+    sys.exit(1)
+
+# This is referenced in setup.py
+from picomc.main import main
