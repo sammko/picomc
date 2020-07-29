eb0098750167cf8b0e80c62134591114c21b268f
Samuel Čavoj <samuel@cavoj.net>
2020-07-29 21:37:38 +0200

    Rewrite downloader_urllib3 as a class

--- picomc/downloader.py
+++ picomc/downloader.py
@@ -14,24 +14,6 @@ import picomc.logging
 from picomc.logging import logger
 
 
-def copyfileobj_prog(fsrc, fdst, callback, stop_event, length=0):
-    if not length:
-        # COPY_BUFSIZE is undocumented and requires python 3.8
-        length = getattr(shutil, "COPY_BUFSIZE", 64 * 1024)
-
-    fsrc_read = fsrc.read
-    fdst_write = fdst.write
-
-    while True:
-        if stop_event.is_set():
-            raise InterruptedError
-        buf = fsrc_read(length)
-        if not buf:
-            break
-        fdst_write(buf)
-        callback(len(buf))
-
-
 @contextmanager
 def DlTempFile(*args, default_mode=0o666, try_delete=True, **kwargs):
     """A NamedTemporaryFile which is created with permissions as per
@@ -52,87 +34,112 @@ def DlTempFile(*args, default_mode=0o666, try_delete=True, **kwargs):
             os.unlink(f.name)
 
 
-def downloader_urllib3(q, size=None, workers=16):
-    http_pool = urllib3.PoolManager(cert_reqs="CERT_REQUIRED", ca_certs=certifi.where())
-    total = len(q)
-    logger.debug("Downloading {} files.".format(total))
-    have_size = size is not None
-    errors = []
-    had_error = False
+class Downloader:
+    def __init__(self, queue, total_size=None, workers=16):
+        self.queue = queue
+        self.total = len(queue)
+        self.known_size = total_size is not None
+        if self.known_size:
+            self.total_size = total_size
+        self.errors = list()
+        self.workers = workers
+        self.fut_to_url = dict()
+        self.http_pool = urllib3.PoolManager(
+            cert_reqs="CERT_REQUIRED", ca_certs=certifi.where()
+        )
+        self.stop_event = threading.Event()
 
-    def dl_file(i, url, dest, sz_callback, stop_event):
+    def copyfileobj_prog(self, fsrc, fdst, callback, length=0):
+        if not length:
+            # COPY_BUFSIZE is undocumented and requires python 3.8
+            length = getattr(shutil, "COPY_BUFSIZE", 64 * 1024)
+
+        fsrc_read = fsrc.read
+        fdst_write = fdst.write
+
+        while True:
+            if self.stop_event.is_set():
+                raise InterruptedError
+            buf = fsrc_read(length)
+            if not buf:
+                break
+            fdst_write(buf)
+            callback(len(buf))
+
+    def download_file(self, i, url, dest, sz_callback):
         # In case the task could not be cancelled
-        if stop_event.is_set():
+        if self.stop_event.is_set():
             raise InterruptedError
         os.makedirs(os.path.dirname(dest), exist_ok=True)
-        logger.debug("Downloading [{}/{}]: {}".format(i, total, url))
-        resp = http_pool.request("GET", url, preload_content=False)
+        logger.debug("Downloading [{}/{}]: {}".format(i, self.total, url))
+        resp = self.http_pool.request("GET", url, preload_content=False)
         if resp.status != 200:
-            errors.append(
-                "Failed to download ({}) [{}/{}]: {}".format(resp.status, i, total, url)
+            self.errors.append(
+                "Failed to download ({}) [{}/{}]: {}".format(
+                    resp.status, i, self.total, url
+                )
             )
             resp.release_conn()
-            return 0
+            return
         with DlTempFile(dir=os.path.dirname(dest), delete=False) as tempf:
-            copyfileobj_prog(resp, tempf, sz_callback, stop_event)
+            self.copyfileobj_prog(resp, tempf, sz_callback)
             tempf.close()
             os.replace(tempf.name, dest)
         resp.release_conn()
-        return len(resp.data)
 
-    disable_progressbar = picomc.logging.debug
-
-    if have_size:
-        cm_progressbar = tqdm(
-            total=size,
-            disable=disable_progressbar,
-            unit_divisor=1024,
-            unit="iB",
-            unit_scale=True,
-        )
-    else:
-        cm_progressbar = tqdm(total=total, disable=disable_progressbar)
-
-    with cm_progressbar as tq, ThreadPoolExecutor(max_workers=workers) as tpe:
-        fut_to_url = dict()
-        stop_event = threading.Event()
-
-        def sz_callback(sz):
-            tq.update(sz)
-
-        for i, (url, dest) in enumerate(q, start=1):
-            cb = sz_callback if have_size else (lambda x: None)
-            fut = tpe.submit(dl_file, i, url, dest, cb, stop_event)
-            fut_to_url[fut] = url
-
+    def reap_future(self, future, tq):
         try:
-            for fut in concurrent.futures.as_completed(fut_to_url.keys()):
-                try:
-                    size = fut.result()
-                except Exception as ex:
-                    logger.error(
-                        "Exception while downloading {}: {}".format(fut_to_url[fut], ex)
-                    )
-                    had_error = True
-                else:
-                    if not have_size:
-                        tq.update(1)
-                    # if we have size, the progress bar was already updated
-                    # from within the thread
-        except KeyboardInterrupt as ex:
-            tq.close()
-            logger.warning("Stopping downloader threads.")
-            stop_event.set()
-            tpe.shutdown()
-            for fut in fut_to_url:
-                fut.cancel()
-            raise ex from None
+            future.result()
+        except Exception as ex:
+            msg = f"Exception while downloading {self.fut_to_url[future]}: {ex}"
+            self.errors.append(msg)
+        else:
+            if not self.known_size:
+                tq.update(1)
+            # if we have size, the progress bar was already updated
+            # from within the thread
 
-    # Do this at the end in order to not break the progress bar.
-    for error in errors:
-        logger.warning(error)
+    def cancel(self, tq, tpe):
+        tq.close()
+        logger.warning("Stopping downloader threads.")
+        self.stop_event.set()
+        tpe.shutdown()
+        for fut in self.fut_to_url:
+            fut.cancel()
 
-    return not errors and not had_error
+    def download(self):
+        logger.debug("Downloading {} files.".format(self.total))
+        disable_progressbar = picomc.logging.debug
+
+        if self.known_size:
+            cm_progressbar = tqdm(
+                total=self.total_size,
+                disable=disable_progressbar,
+                unit_divisor=1024,
+                unit="iB",
+                unit_scale=True,
+            )
+        else:
+            cm_progressbar = tqdm(total=self.total, disable=disable_progressbar)
+
+        with cm_progressbar as tq, ThreadPoolExecutor(max_workers=self.workers) as tpe:
+            for i, (url, dest) in enumerate(self.queue, start=1):
+                cb = tq.update if self.known_size else (lambda x: None)
+                fut = tpe.submit(self.download_file, i, url, dest, cb)
+                self.fut_to_url[fut] = url
+
+            try:
+                for fut in concurrent.futures.as_completed(self.fut_to_url.keys()):
+                    self.reap_future(fut, tq)
+            except KeyboardInterrupt as ex:
+                self.cancel(tq, tpe)
+                raise ex from None
+
+        # Do this at the end in order to not break the progress bar.
+        for error in self.errors:
+            logger.error(error)
+
+        return not self.errors
 
 
 class DownloadQueue:
@@ -153,4 +160,4 @@ class DownloadQueue:
     def download(self):
         if not self.q:
             return True
-        return downloader_urllib3(self.q, size=self.size)
+        return Downloader(self.q, total_size=self.size).download()
