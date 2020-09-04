57359685daf606b5f685293c3e43b2e3697e6e43
Samuel Čavoj <samuel@cavoj.net>
2020-09-04 13:04:13 +0200

    Remove pointless del statement which confused linter

--- picomc/__init__.py
+++ picomc/__init__.py
@@ -1,13 +1,9 @@
 import sys
 
+from picomc._version import __version__
 from picomc.cli import picomc_cli
 
-from ._version import __version__
-
-del _version
-
 MINPYVERSION = (3, 7, 0)
-
 
 if sys.version_info < MINPYVERSION:
     print("picomc, version {}".format(__version__))
